0bfd633bffc0e55b68b477444f4cab026ec7f5ba
Jakub Pazdyga <admin@lascalia.com>
2015-12-22 12:43:51 +0100

    Moved LB from boto2 to boto3

--- create_load_balancer.py
+++ create_load_balancer.py
@@ -1,21 +1,49 @@
-import os
-import boto.ec2.elb
-from boto.ec2.elb import ELBConnection
-from boto.ec2.elb import HealthCheck
-from pprint import pprint
+import boto3
 
-conn = ELBConnection()
+conn = boto3.client('elb')
+lbname = 'test1-loadbalancer';
 
 def list ():
- list_load_balancers = conn.get_all_load_balancers()
+ list_load_balancers = conn.describe_load_balancers(
+  LoadBalancerNames=[
+   lbname,
+  ],
+ )
  print(list_load_balancers);
 
-list();
-
 def create():
+ listeners = [
+  {'Protocol': 'http',
+   'LoadBalancerPort': 80,
+   'InstanceProtocol': 'http',
+   'InstancePort': 80,
+  }];
  zones = ['eu-west-1a','eu-west-1b','eu-west-1c']
- ports = [(80,80,'http')]
  security_groups = ['sg-de04e3ba']
- lb = conn.create_load_balancer('test1-loadbalancer', zones, ports, security_groups=security_groups)
+ lb = conn.create_load_balancer(
+  LoadBalancerName=lbname, 
+  Listeners=listeners, 
+  AvailabilityZones=zones,
+  SecurityGroups=security_groups)
 
+def setupattr():
+ conn.modify_load_balancer_attributes(
+  LoadBalancerName=lbname,
+  LoadBalancerAttributes={
+   'CrossZoneLoadBalancing': {
+    'Enabled': True,
+   },
+  }
+ )
+
+def setupstick():
+ conn.create_lb_cookie_stickiness_policy(
+  LoadBalancerName=lbname,
+  PolicyName='DefaultLBStickiness-1',
+  CookieExpirationPeriod=10,
+ )
+
+#list();
 create();
+setupattr();
+setupstick();
