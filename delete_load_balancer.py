5a82928a714005cae34d63945fa50d4f820b9a9e
Jakub Pazdyga <admin@lascalia.com>
2015-12-24 15:54:24 +0100

    Pushing today's work

--- delete_load_balancer.py
+++ delete_load_balancer.py
@@ -1,14 +1,37 @@
 import boto3
 import json
-import argparse
+import sys
 import list_load_balancers
 from pprint import pprint
 
 conn = boto3.client('elb')
 
-#parser = argparse.ArgumentParser(description='Get the AWS Load Balancer details.')
-#parser.add_argument('loadbalancer', metavar='lbname', type=str, nargs='+', help='Load balancer name')
-#args = parser.parse_args()
-#lbname = args.loadbalancer[0];
+def lista():
+ lista.lblist=list_load_balancers.array;
 
-list();
+def delete():
+ conn.delete_load_balancer(
+  LoadBalancerName=lb
+ )
+
+def interactive():
+ num=1
+ print("\nChoose LB to delete:\n");
+ for i in lista.lblist:
+  print(num, i);
+  num=num+1 
+ lbn=input("Type the number only: ");
+ slbn=int(lbn)
+ index=slbn-1
+ lb=list_load_balancers.array[index]
+
+if len(sys.argv) > 1:
+ lb = sys.argv[1];
+ lista()
+ if lb in lista.lblist:
+  delete()
+ else:
+  print("You specified non-existing LB name. Please re-check this or run", sys.argv[0], "again.");
+else:
+ lista()
+ interactive()
--- list_load_balancers.py
+++ list_load_balancers.py
@@ -1,16 +1,14 @@
 import boto3
 
 conn = boto3.client('elb')
+array = []
 
-def list():
- list_load_balancers = conn.describe_load_balancers(
-  LoadBalancerNames=[],
-  PageSize=20,
- )
- test = list_load_balancers.get('LoadBalancerDescriptions')
- entity = 0;
- for entity in test:
-  val1 = entity.get('LoadBalancerName');
-  print(val1);
-
-list();
+list_load_balancers = conn.describe_load_balancers(
+ LoadBalancerNames=[],
+ PageSize=20,
+)
+test = list_load_balancers.get('LoadBalancerDescriptions')
+entity = 0;
+for entity in test:
+ val1 = entity.get('LoadBalancerName');
+ array.append(val1)
